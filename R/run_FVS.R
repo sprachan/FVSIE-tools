8e97b8eb3a0d97701740bba9be7d43a7bf400f20
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-03-24 14:16:47 -0600

    feat: automatically subset treelist in run_FVS()
    - run_FVS() automatically subsets the input treelist to match the
      inputted stand's STAND_CN
    - If there are no (live) trees available to grow, messages and returns
      NULL.

--- R/run_FVS.R
+++ R/run_FVS.R
@@ -30,45 +30,49 @@ run_FVS <- function(tree_list, stand_info, out_dir, fvs_bin, ..., verbose = FALS
   if(!file.exists(file.path(fvs_bin, 'FVSie.dll'))){
     stop('FVSie.dll not found in ', fvs_bin, '. Check FVS installation.')
   }
-
   if(!('fvs.TREE_ID' %in% colnames(tree_list))){
     tree_list$fvs.TREE_ID <- 1:nrow(tree_list)
   }
 
-  rFVS::fvsLoad("FVSie", fvs_bin)
-  f <- write_FVS_files(tree_list = tree_list, stand_info = stand_info,
-                      out_dir = out_dir, ...)
+  tr <- as.data.frame(tree_list[tree_list$STAND_CN == stand_info$STAND_CN,])
+  if(sum(tr$HISTORY %in% 6:9) == nrow(tr)|nrow(tr) == 0){
+    message('Skipping stand ', stand_info$STAND_CN, sep = '')
+    return(list(tree_list = NULL, summary = NULL))
+  }else{
+    rFVS::fvsLoad("FVSie", fvs_bin)
+    f <- write_FVS_files(tree_list = tr, stand_info = stand_info,
+                         out_dir = out_dir, ...)
 
-  rFVS::fvsSetCmdLine(paste0('--keywordfile=', f, '.key'))
+    rFVS::fvsSetCmdLine(paste0('--keywordfile=', f, '.key'))
 
-  fvs_output <- rFVS::fvsInteractRun(AfterEM1 = 'rFVSIEtools::fetch_trees()',
-                                     SimEnd = rFVS::fvsGetSummary)
+    fvs_output <- rFVS::fvsInteractRun(AfterEM1 = 'rFVSIEtools::fetch_trees()',
+                                       SimEnd = rFVS::fvsGetSummary)
 
-  if(verbose) print(names(fvs_output))
+    if(verbose) print(names(fvs_output))
 
-  # year 0 tree list
-  tl0 <- dplyr::left_join(fvs_output[[1]]$AfterEM1,
-                          dplyr::select(tree_list, .data$fvs.TREE_ID, .data$TUID,
-                                        .data$PID),
-                          by = c('id' = 'fvs.TREE_ID'))
+    # year 0 tree list
+    tl0 <- dplyr::left_join(fvs_output[[1]]$AfterEM1,
+                            dplyr::select(tr, .data$fvs.TREE_ID, .data$TUID,
+                                          .data$PID),
+                            by = c('id' = 'fvs.TREE_ID'))
 
-  # end of projection cycle tree list
-  tl1 <- dplyr::left_join(fvs_output[[2]]$AfterEM1,
-                          dplyr::select(tree_list, .data$fvs.TREE_ID, .data$TUID,
-                                        .data$PID),
-                          by = c('id' = 'fvs.TREE_ID'))
+    # end of projection cycle tree list
+    tl1 <- dplyr::left_join(fvs_output[[2]]$AfterEM1,
+                            dplyr::select(tr, .data$fvs.TREE_ID, .data$TUID,
+                                          .data$PID),
+                            by = c('id' = 'fvs.TREE_ID'))
 
-  if(verbose) cat('Year 0 nrow: ', nrow(tl0), '\n Year N nrow: ', nrow(tl1))
+    if(verbose) cat('Year 0 nrow: ', nrow(tl0), '\n Year N nrow: ', nrow(tl1))
 
-  tl <- rbind(tl0, tl1)
-  plt_summary <- cbind(fvs_output[[length(fvs_output)]],
-                       data.frame(PID = stand_info$PID))
+    tl <- rbind(tl0, tl1)
+    plt_summary <- cbind(fvs_output[[length(fvs_output)]],
+                         data.frame(PID = stand_info$PID))
 
-  rFVS::fvsLoad('FVSie', fvs_bin)
+    rFVS::fvsLoad('FVSie', fvs_bin)
 
-  list(tree_list = tl, summary = plt_summary)
+    list(tree_list = tl, summary = plt_summary)
+  }
 }
-
 #' Get treelist from FVS run.
 #'
 #' Exported only because of a quirk in rFVS.
