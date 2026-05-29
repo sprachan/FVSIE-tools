bc3f431377fd62f2eb82c8f8ac7ff5ae0abd80c3
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-05-29 10:48:27 -0600

    feat: example stand info and tree list

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Example Stand Information Table
+#'
+#' A subset of FIA data for just one plot in western Montana.
+#' These data come from the FVS_STANDINIT_PLOT table and have been processed using [get_FIA_state()]
+#' with the option "add_identifiers" set to TRUE.
+#'
+#' @format ## `ex_stand`
+#' A data frame with 1 rows and 34 columns.
+"ex_stand"
+
+#' Example Tree list
+#'
+#' A subset of FIA data for just one plot in western Montana.
+#' These data come from the FVS_TREEINIT_PLOT table and have been processed using [get_FIA_state()]
+#' with the option "add_identifiers" set to TRUE.
+#'
+#' @format ## `ex_trees`
+#' A data frame with 51 rows and 31 columns.
+"ex_trees"
+
+
