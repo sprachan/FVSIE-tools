c373f6fce77ca87f3035c491cc1ccbfd5102cbea
Sejal Prachand <sp25846@umconnect.umt.edu>
2026-07-20 12:58:44 -0600

    docs, feat: indenting, create output db if required

--- R/get_FIA_state.R
+++ R/get_FIA_state.R
@@ -1,15 +1,26 @@
 #' Get FVS-ready FIA data from a state-level database
 #'
-#' `get_FIA_state()` fetches FVS Stand and FVS Tree tables from a downloaded state-level FIA
-#' database (from the [FIA datamart website](https://apps.fs.usda.gov/fia/datamart/datamart.html)). Note that these tables are only available at the state-level, so
-#' this function only works with state-level databases.
+#' `get_FIA_state()` fetches FVS Stand and FVS Tree tables from a downloaded
+#' state-level FIA database (from the [FIA datamart
+#' website](https://apps.fs.usda.gov/fia/datamart/datamart.html)). Note that
+#' these tables are only available at the state-level, so this function only
+#' works with state-level databases.
 #'
 #' @param database Character string. Location for the FIA database.
 #' @param fia_cond_subset Dataframe. A subset of an FIA COND table.
 #' @param verbose Boolean value. If TRUE, will print SQL queries to console.
-#' @param add_identifiers Boolean value. If TRUE, will add a PID (Plot IDentifier) column to the stand table and a TUID (Tree Unique IDentifier) column to the tree table. PID and TUID are unique, persistent identifiers. They uniquely identify each FIA plot and each tree in each plot. Unlike FIA-provided identifiers, these stay the same across all years.
+#' @param add_identifiers Boolean value. If TRUE, will add a PID (Plot
+#'   IDentifier) column to the stand table and a TUID (Tree Unique IDentifier)
+#'   column to the tree table. PID and TUID are unique, persistent identifiers.
+#'   They uniquely identify each FIA plot and each tree in each plot. Unlike
+#'   FIA-provided identifiers, these stay the same across all years.
+#' @param output_database Character string. Location for filtered data tables.
+#'   Default is 'fvs_ready.db'
 #'
-#' @returns `get_FIA_state()`: List of 2. $FVS_StandInit is a dataframe of the stand information. $FVS_TreeInit is a dataframe of all tree measurements. A single stand from this list selected with STAND_CN and the associated tree list (matching STAND_CN) can be passed to `run_FVS()`.
+#' @returns `get_FIA_state()`: List of 2. $FVS_StandInit is a dataframe of the
+#'   stand information. $FVS_TreeInit is a dataframe of all tree measurements. A
+#'   single stand from this list selected with STAND_CN and the associated tree
+#'   list (matching STAND_CN) can be passed to `run_FVS()`.
 #' @examples
 #'
 #' # get just the condition table:
@@ -27,7 +38,7 @@
 #'
 #' @export
 get_FIA_state <- function(database, fia_cond_subset, verbose = FALSE,
-                          add_identifiers = FALSE){
+                          add_identifiers = FALSE, output_database = 'fvs_ready.db'){
   on.exit(try(DBI::dbDisconnect(fia_db_conn)), add = TRUE)
   check_db_get(database)
   fia_db_conn <- DBI::dbConnect(RSQLite::SQLite(), database)
@@ -66,5 +77,10 @@ get_FIA_state <- function(database, fia_cond_subset, verbose = FALSE,
     tree_init <- add_tuid(stand_init, tree_init)
   }
 
+  if(!file.exists(output_database)){file.create(output_database)}
+  conn <- DBI::dbConnect(RSQLite::SQLite(), output_database)
+  DBI::dbWriteTable(conn, 'FVS_StandInit', stand_init, overwrite = TRUE)
+  DBI::dbWriteTable(conn, 'FVS_TreeInit', tree_init, overwrite = TRUE)
+  DBI::dbDisconnect(conn)
   return(list(FVS_StandInit = stand_init, FVS_TreeInit = tree_init))
 }
